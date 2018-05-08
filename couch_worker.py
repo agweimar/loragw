da719b830e0a31b8173ac4fba0fa3b49ab457fb6
Leonard Pollak <leonard.pollak@gmail.com>
2018-05-08 09:46:18 +0000

    couch worker up

--- couch_worker.py
+++ couch_worker.py
@@ -3,20 +3,20 @@ import sys
 import time
 import base64
 import json
+import zmq
 from cloudant import couchdb
 from  multiprocessing import Process
 
-COUCH_BACK_PORT = 7778
+WORKER_COUNT = 3
 
 COUCH_BACK_IP = "loragw1"
+COUCH_BACK_PORT = 7778
 
 COUCHDB_HOST = 'http://localhost:5984'
 COUCHDB_USERNAME = ""
 COUCHDB_PASSWORD = ""
 
-WORKER_COUNT = 1
-
-def couch_worker(couch_back_ip, couch_back_port):
+def couch_worker(couch_back_ip, couch_back_port, worker_id):
     # gets all data
     # puts them into couchdb
     context = zmq.Context()
@@ -27,15 +27,34 @@ def couch_worker(couch_back_ip, couch_back_port):
     # TODO
     while True:
         message = rep.recv()
+        couch_document = {
+                        data
+                        }
         #print("Received request: ", message)
-        # -> to couchdb
-        time.sleep(1)
-        rep.send_string("processed: %s" % message)
+	# open couchdb connection
+        with couchdb(COUCHDB_USERNAME, COUCHDB_PASSWORD, url=COUCHDB_HOST) as client:
+            # Perform client tasks...
+            session = client.session()
+            print('Username: {0}'.format(session['userCtx']['name']))
+            #print('Databases: {0}'.format(client.all_dbs()))
+            
+            # Create a database
+            print("creating database")
+            #node_db = client.create_database(node_id)
+            node_db = client['lora_test']
+            
+            if node_db.exists():
+                doc_created = node_db.create_document(couch_document)
+                
+            if doc_created.exists():
+                print("node data saved in couchdb")
+                rep.send_string("Worker #%d processed: %s" % (worker_id, message))
+	
 
 def main():
     for worker in range(WORKER_COUNT):
         print("Starting worker #%d" % worker)
-        Process(target=couch_worker, args=(COUCH_BACK_IP, COUCH_BACK_PORT)).start()
+        Process(target=couch_worker, args=(COUCH_BACK_IP, COUCH_BACK_PORT, worker)).start()
 
 if __name__ == '__main__':
 	main()

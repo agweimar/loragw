6f86b7f872027438003cdcae3e399825124b7fb4
Leonard Pollak <leonard.pollak@gmail.com>
2018-05-08 13:47:27 +0000

    couch worker update

--- couch_worker.py
+++ couch_worker.py
@@ -17,32 +17,31 @@ def couch_worker(couch_back_ip, couch_back_port, worker_id):
     rep = context.socket(zmq.REP)
     rep.connect("tcp://%s:%s" % (couch_back_ip, couch_back_port))
 
-    server_id=1
     # TODO
     while True:
         message = rep.recv()
-        print(message)
-        couch_document = {}
+        couch_document = json.loads(message.decode().split(' ',1)[1])
+        print(couch_document)
+	
         #print("Received request: ", message)
 	# open couchdb connection
-        #with couchdb(COUCHDB_USERNAME, COUCHDB_PASSWORD, url=COUCHDB_HOST) as client:
-        #    # Perform client tasks...
-        #    session = client.session()
-        #    print('Username: {0}'.format(session['userCtx']['name']))
-        #    #print('Databases: {0}'.format(client.all_dbs()))
-        #    
-        #    # Create a database
-        #    print("creating database")
-        #    #node_db = client.create_database(node_id)
-        #    node_db = client['lora_test']
-        #    
-        #    if node_db.exists():
-        #        doc_created = node_db.create_document(couch_document)
-        #        
-        #    if doc_created.exists():
-        #        print("node data saved in couchdb")
-        #        rep.send_string("Worker #%d processed: %s" % (worker_id, message))
-	
+        with couchdb(COUCHDB_USERNAME, COUCHDB_PASSWORD, url=COUCHDB_HOST) as client:
+            # Perform client tasks...
+            session = client.session()
+            print('Username: {0}'.format(session['userCtx']['name']))
+            #print('Databases: {0}'.format(client.all_dbs()))
+            
+            # Create a database
+            print("creating database")
+            #node_db = client.create_database(node_id)
+            node_db = client['lora_test']
+            
+            if node_db.exists():
+                doc_created = node_db.create_document(couch_document)
+                
+            if doc_created.exists():
+                print("node data saved in couchdb")
+                rep.send_string("Worker #%d processed: %s" % (worker_id, message))
 
 def main():
     for worker in range(WORKER_COUNT):
